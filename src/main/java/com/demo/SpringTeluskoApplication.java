c98a07beb45278ba4135c0611ac238faa92e6b01
phani522 <phanikanthjonnalagadda663@gmail.com>
2023-12-07 15:17:51 -0600

    worst commit

---
+++ src/main/java/com/demo/SpringTeluskoApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTeluskoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("main ");
+		SpringApplication.run(SpringTeluskoApplication.class, args);
+	}
+
+}
